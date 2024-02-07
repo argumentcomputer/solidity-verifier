b2c12a08f9a53099b49aee355fe23e04a0d9d7dd
Artem Storozhuk <storojs72@gmail.com>
2024-02-07 15:53:13 +0200

    IPA over Grumpkin implementation as a library

---
+++ src/blocks/IpaPcs.sol
@@ -0,0 +1,324 @@
+// SPDX-License-Identifier: Apache-2.0
+pragma solidity ^0.8.16;
+
+import "src/blocks/grumpkin/Grumpkin.sol";
+import "src/blocks/KeccakTranscript.sol";
+
+library InnerProductArgument {
+    struct IpaInputGrumpkin {
+        Grumpkin.GrumpkinAffinePoint[] ck_v;
+        Grumpkin.GrumpkinAffinePoint[] ck_s;
+        uint256[] point;
+        Grumpkin.GrumpkinAffinePoint[] L_vec;
+        Grumpkin.GrumpkinAffinePoint[] R_vec;
+        Grumpkin.GrumpkinAffinePoint commitment;
+        uint256 eval;
+        uint256 a_hat;
+    }
+
+    struct InstanceGrumpkin {
+        Grumpkin.GrumpkinAffinePoint comm_a_vec;
+        uint256[] b_vec;
+        uint256 c;
+    }
+
+    struct R {
+        uint256[] r_vec;
+        uint256[] r_vec_squared;
+        uint256[] r_vec_inversed;
+        uint256[] r_vec_inversed_squared;
+    }
+
+    struct P_hat_right_input {
+        uint256 n;
+        R r_vectors;
+        Grumpkin.GrumpkinAffinePoint[] ck1;
+        uint256[] b_vec;
+        uint256 a_hat;
+        Grumpkin.GrumpkinAffinePoint ck_c;
+    }
+
+    function batchInvert(uint256[] memory r_vec, uint256 modulus) private view returns (uint256[] memory) {
+        uint256[] memory products = new uint256[](r_vec.length);
+        uint256 acc = 1;
+        uint256 index;
+        for (index = 0; index < r_vec.length; index++) {
+            products[index] = acc;
+            acc = mulmod(acc, r_vec[index], modulus);
+        }
+
+        acc = Field.invert(acc, modulus);
+
+        uint256[] memory inversed = new uint256[](r_vec.length);
+
+        uint256 tmp;
+        for (index = 0; index < r_vec.length; index++) {
+            tmp = mulmod(acc, r_vec[r_vec.length - index - 1], modulus);
+            inversed[r_vec.length - index - 1] = mulmod(products[r_vec.length - index - 1], acc, modulus);
+            acc = tmp;
+        }
+
+        return inversed;
+    }
+
+    function compute_r_based_values(uint256[] memory r_vec, uint256 modulus) private view returns (R memory) {
+        uint256[] memory r_vec_squared = new uint256[](r_vec.length);
+        uint256 index;
+        for (index = 0; index < r_vec.length; index++) {
+            r_vec_squared[index] = mulmod(r_vec[index], r_vec[index], modulus);
+        }
+
+        uint256[] memory r_vec_inversed = batchInvert(r_vec, modulus);
+
+        uint256[] memory r_vec_inversed_squared = new uint256[](r_vec.length);
+        for (index = 0; index < r_vec.length; index++) {
+            r_vec_inversed_squared[index] = mulmod(r_vec_inversed[index], r_vec_inversed[index], modulus);
+        }
+        return R(r_vec, r_vec_squared, r_vec_inversed, r_vec_inversed_squared);
+    }
+
+    function split_at(Grumpkin.GrumpkinAffinePoint[] memory ck, uint256 n)
+        private
+        pure
+        returns (Grumpkin.GrumpkinAffinePoint[] memory, Grumpkin.GrumpkinAffinePoint[] memory)
+    {
+        require(n <= ck.length, "[split_at] unexpected n");
+
+        Grumpkin.GrumpkinAffinePoint[] memory ck1 = new Grumpkin.GrumpkinAffinePoint[](n);
+        Grumpkin.GrumpkinAffinePoint[] memory ck2 = new Grumpkin.GrumpkinAffinePoint[](n);
+        uint256 ck_index = 0;
+        for (uint256 i = 0; i < n; i++) {
+            ck1[i] = ck[ck_index];
+            ck_index++;
+        }
+        for (uint256 i = n; i < ck.length; i++) {
+            ck2[i] = ck[ck_index];
+            ck_index++;
+        }
+
+        return (ck1, ck2);
+    }
+
+    function scale(Grumpkin.GrumpkinAffinePoint[] memory ck_c, uint256 r)
+        private
+        view
+        returns (Grumpkin.GrumpkinAffinePoint memory)
+    {
+        require(ck_c.length == 1, "[scale] unexpected ck_c");
+        return Grumpkin.scalarMul(ck_c[0], r);
+    }
+
+    function inner_product_inner(uint256[] memory c) private pure returns (uint256[] memory) {
+        if (c.length == 1) {
+            return c;
+        }
+        uint256[] memory c_inner = new uint256[](c.length / 2);
+        for (uint256 index = 0; index < c_inner.length; index++) {
+            c_inner[index] = addmod(c[2 * index], c[2 * index + 1], Grumpkin.P_MOD);
+        }
+        return inner_product_inner(c_inner);
+    }
+
+    function inner_product(uint256[] memory a, uint256[] memory b) private pure returns (uint256) {
+        require(a.length == b.length);
+        uint256[] memory c = new uint256[](a.length);
+        uint256 index;
+        for (index = 0; index < a.length; index++) {
+            c[index] = mulmod(a[index], b[index], Grumpkin.P_MOD);
+        }
+
+        c = inner_product_inner(c);
+        return c[0];
+    }
+
+    function get_pos_value(uint256 i) private pure returns (uint256) {
+        require(i >= 1, "[get_pos_value], i < 1");
+        require(i <= 16, "[get_pos_value], i > 16");
+        uint256[] memory result = new uint256[](16);
+        result[0] = 0;
+        result[1] = 1;
+        result[2] = 1;
+        result[3] = 2;
+        result[4] = 2;
+        result[5] = 2;
+        result[6] = 2;
+        result[7] = 3;
+        result[8] = 3;
+        result[9] = 3;
+        result[10] = 3;
+        result[11] = 3;
+        result[12] = 3;
+        result[13] = 3;
+        result[14] = 3;
+        result[15] = 4;
+        return result[i - 1];
+    }
+
+    function compute_P_hat_right(P_hat_right_input memory input)
+        private
+        returns (Grumpkin.GrumpkinAffinePoint memory)
+    {
+        uint256[] memory s = new uint256[](input.n);
+
+        uint256 v = 1;
+        uint256 index;
+        for (index = 0; index < input.r_vectors.r_vec_inversed.length; index++) {
+            v = mulmod(v, input.r_vectors.r_vec_inversed[index], Grumpkin.P_MOD);
+        }
+        s[0] = v;
+
+        uint256 pos_in_r;
+        uint256 r_square_length = input.r_vectors.r_vec_squared.length;
+        for (index = 1; index < input.n; index++) {
+            pos_in_r = get_pos_value(index);
+            s[index] = mulmod(
+                s[index - (1 << pos_in_r)],
+                input.r_vectors.r_vec_squared[r_square_length - 1 - pos_in_r],
+                Grumpkin.P_MOD
+            );
+        }
+
+        uint256 b_hat = inner_product(input.b_vec, s);
+        Grumpkin.GrumpkinAffinePoint memory ck_hat = Grumpkin.multiScalarMul(input.ck1, s);
+
+        Grumpkin.GrumpkinAffinePoint[] memory bases = new Grumpkin.GrumpkinAffinePoint[](2);
+        bases[0] = ck_hat;
+        bases[1] = input.ck_c;
+
+        uint256[] memory scalars = new uint256[](2);
+        scalars[0] = input.a_hat;
+        scalars[1] = mulmod(input.a_hat, b_hat, Grumpkin.P_MOD);
+
+        return Grumpkin.multiScalarMul(bases, scalars);
+    }
+
+    function compute_P_hat_left(IpaInputGrumpkin memory input, R memory r_vec, Grumpkin.GrumpkinAffinePoint memory ck_c)
+        private
+        returns (Grumpkin.GrumpkinAffinePoint memory)
+    {
+        Grumpkin.GrumpkinAffinePoint memory P = Grumpkin.add(input.commitment, Grumpkin.scalarMul(ck_c, input.eval));
+
+        uint256 msm_len = input.L_vec.length + input.R_vec.length + 1;
+
+        uint256 msm_index = 0;
+        uint256[] memory scalars = new uint256[](msm_len);
+        for (uint256 index = 0; index < r_vec.r_vec_squared.length; index++) {
+            scalars[msm_index] = r_vec.r_vec_squared[index];
+            msm_index++;
+        }
+        for (uint256 index = 0; index < r_vec.r_vec_inversed_squared.length; index++) {
+            scalars[msm_index] = r_vec.r_vec_inversed_squared[index];
+            msm_index++;
+        }
+        scalars[msm_index] = 0x01;
+
+        msm_index = 0;
+        Grumpkin.GrumpkinAffinePoint[] memory bases = new Grumpkin.GrumpkinAffinePoint[](msm_len);
+        for (uint256 index = 0; index < input.L_vec.length; index++) {
+            bases[msm_index] = input.L_vec[index];
+            msm_index++;
+        }
+        for (uint256 index = 0; index < input.R_vec.length; index++) {
+            bases[msm_index] = input.R_vec[index];
+            msm_index++;
+        }
+        bases[msm_index] = P;
+
+        return Grumpkin.multiScalarMul(bases, scalars);
+    }
+
+    function compute_P_hat_right(
+        uint256 b_hat,
+        uint256 a_hat,
+        Grumpkin.GrumpkinAffinePoint memory ck_hat,
+        Grumpkin.GrumpkinAffinePoint memory ck_c
+    ) private returns (Grumpkin.GrumpkinAffinePoint memory) {
+        Grumpkin.GrumpkinAffinePoint[] memory bases = new Grumpkin.GrumpkinAffinePoint[](2);
+        bases[0] = ck_hat;
+        bases[1] = ck_c;
+
+        uint256[] memory scalars = new uint256[](2);
+        scalars[0] = a_hat;
+        scalars[1] = mulmod(a_hat, b_hat, Grumpkin.P_MOD);
+
+        return Grumpkin.multiScalarMul(bases, scalars);
+    }
+
+    function verifyGrumpkin(IpaInputGrumpkin memory input, KeccakTranscriptLib.KeccakTranscript memory transcript)
+        public
+        returns (bool)
+    {
+        uint256 n = 2 ** input.point.length;
+
+        uint256[] memory b_vec = EqPolynomialLib.evals(input.point, Grumpkin.P_MOD, Grumpkin.negateBase);
+        (Grumpkin.GrumpkinAffinePoint[] memory ck1,) = split_at(input.ck_v, b_vec.length);
+
+        // b"IPA" in Rust
+        uint8[] memory label = new uint8[](3);
+        label[0] = 0x49;
+        label[1] = 0x50;
+        label[2] = 0x41;
+
+        transcript = KeccakTranscriptLib.dom_sep(transcript, label);
+
+        if (b_vec.length != n) {
+            revert("NovaError::InvalidInputLength");
+        }
+        if (n != 1 << input.L_vec.length) {
+            revert("NovaError::InvalidInputLength");
+        }
+        if (input.L_vec.length != input.R_vec.length) {
+            revert("NovaError::InvalidInputLength");
+        }
+        if (input.L_vec.length >= 32) {
+            revert("NovaError::InvalidInputLength");
+        }
+
+        // b"U" in Rust
+        label = new uint8[](1);
+        label[0] = 0x55;
+
+        transcript =
+            KeccakTranscriptLib.absorb(transcript, label, InstanceGrumpkin(input.commitment, b_vec, input.eval));
+
+        // b"r" in Rust
+        label = new uint8[](1);
+        label[0] = 0x72;
+        uint256 r;
+        (transcript, r) = KeccakTranscriptLib.squeeze(transcript, ScalarFromUniformLib.curveGrumpkin(), label);
+
+        uint256[] memory r_vec = new uint256[](input.L_vec.length);
+        for (uint256 index = 0; index < r_vec.length; index++) {
+            // b"L" in Rust
+            label[0] = 0x4c;
+            transcript = KeccakTranscriptLib.absorb(transcript, label, input.L_vec[index].x);
+
+            // b"R" in Rust
+            label[0] = 0x52;
+            transcript = KeccakTranscriptLib.absorb(transcript, label, input.R_vec[index].x);
+
+            // b"r" in Rust
+            label[0] = 0x72;
+            (transcript, r_vec[index]) =
+                KeccakTranscriptLib.squeeze(transcript, ScalarFromUniformLib.curveGrumpkin(), label);
+        }
+
+        R memory r_vectors = compute_r_based_values(r_vec, Grumpkin.P_MOD);
+
+        Grumpkin.GrumpkinAffinePoint memory ck_c = scale(input.ck_s, r);
+
+        Grumpkin.GrumpkinAffinePoint memory P_hat_right =
+            compute_P_hat_right(P_hat_right_input(n, r_vectors, ck1, b_vec, input.a_hat, ck_c));
+
+        Grumpkin.GrumpkinAffinePoint memory P_hat_left = compute_P_hat_left(input, r_vectors, ck_c);
+
+        if (P_hat_right.x != P_hat_left.x) {
+            return false;
+        }
+        if (P_hat_right.y != P_hat_left.y) {
+            return false;
+        }
+
+        return true;
+    }
+}
--- test/keccak-transcript-tests.t.sol
+++ test/keccak-transcript-tests.t.sol
@@ -79,6 +79,14 @@ contract KeccakTranscriptContractTest is Test {
         uint256 scalarVesta = ScalarFromUniformLib.scalarFromUniform(uniform, ScalarFromUniformLib.curveVesta());
         uint256 expectedVesta = 0x0c34755d6b4566f930b8371afd2e4818ae49878a10527fd4443dbec811582d43;
         assertEq(scalarVesta, expectedVesta);
+
+        uint256 scalarBn256 = ScalarFromUniformLib.scalarFromUniform(uniform, ScalarFromUniformLib.curveBn256());
+        uint256 expectedBn256 = 0x02c64acce6367d59e231e9333e02a497b617fe0fffe49e395b8601550d16f993;
+        assertEq(scalarBn256, expectedBn256);
+
+        uint256 scalarGrumpkin = ScalarFromUniformLib.scalarFromUniform(uniform, ScalarFromUniformLib.curveGrumpkin());
+        uint256 expectedGrumpkin = 0x04876bd2613243de5a5e8cce6f8e3d1e3adbb695c1b596ed476b96711bec2313;
+        assertEq(scalarGrumpkin, expectedGrumpkin);
     }
 
     // Following test has been ported: https://github.com/lurk-lab/Nova/blob/solidity-verifier-pp-spartan/src/provider/keccak.rs#L138
@@ -179,5 +187,239 @@ contract KeccakTranscriptContractTest is Test {
 
         expected = Field.reverse256(0xe3585da385704879ec03ef201dbf228e7b227a5af709f83f3ed5f92a5037d633);
         assertEq(output, expected);
+    }
+
+    function testKeccakTranscriptBn256() public {
+        uint8[] memory input = new uint8[](4); // b"test" in Rust
+        input[0] = 0x74;
+        input[1] = 0x65;
+        input[2] = 0x73;
+        input[3] = 0x74;
+
+        KeccakTranscriptLib.KeccakTranscript memory transcript = KeccakTranscriptLib.instantiate(input);
+
+        uint256 input1 = 2;
+        uint8[] memory label1 = new uint8[](2); // b"s1" in Rust
+        label1[0] = 0x73;
+        label1[1] = 0x31;
+
+        uint256 input2 = 5;
+        uint8[] memory label2 = new uint8[](2); // b"s2" in Rust
+        label2[0] = 0x73;
+        label2[1] = 0x32;
+
+        transcript = KeccakTranscriptLib.absorb(transcript, label1, input1);
+        transcript = KeccakTranscriptLib.absorb(transcript, label2, input2);
+
+        uint8[] memory squeezeLabel = new uint8[](2); // b"c1" in Rust
+        squeezeLabel[0] = 0x63;
+        squeezeLabel[1] = 0x31;
+
+        ScalarFromUniformLib.Curve curve = ScalarFromUniformLib.curveBn256();
+        uint256 output;
+        (transcript, output) = KeccakTranscriptLib.squeeze(transcript, curve, squeezeLabel);
+
+        uint256 expected = Field.reverse256(0x9fb71e3b74bfd0b60d97349849b895595779a240b92a6fae86bd2812692b6b0e);
+        assertEq(output, expected);
+
+        uint256 s3 = 128;
+        uint8[] memory label3 = new uint8[](2); // b"s3" in Rust
+        label3[0] = 0x73;
+        label3[1] = 0x33;
+
+        transcript = KeccakTranscriptLib.absorb(transcript, label3, s3);
+
+        // b"c2" in Rust
+        squeezeLabel[0] = 0x63;
+        squeezeLabel[1] = 0x32;
+        (, output) = KeccakTranscriptLib.squeeze(transcript, curve, squeezeLabel);
+
+        expected = Field.reverse256(0xbfd4c50b7d6317e9267d5d65c985eb455a3561129c0b3beef79bfc8461a84f18);
+        assertEq(output, expected);
+    }
+
+    function testKeccakTranscriptGrumpkin() public {
+        uint8[] memory input = new uint8[](4); // b"test" in Rust
+        input[0] = 0x74;
+        input[1] = 0x65;
+        input[2] = 0x73;
+        input[3] = 0x74;
+
+        KeccakTranscriptLib.KeccakTranscript memory transcript = KeccakTranscriptLib.instantiate(input);
+
+        uint256 input1 = 2;
+        uint8[] memory label1 = new uint8[](2); // b"s1" in Rust
+        label1[0] = 0x73;
+        label1[1] = 0x31;
+
+        uint256 input2 = 5;
+        uint8[] memory label2 = new uint8[](2); // b"s2" in Rust
+        label2[0] = 0x73;
+        label2[1] = 0x32;
+
+        transcript = KeccakTranscriptLib.absorb(transcript, label1, input1);
+        transcript = KeccakTranscriptLib.absorb(transcript, label2, input2);
+
+        uint8[] memory squeezeLabel = new uint8[](2); // b"c1" in Rust
+        squeezeLabel[0] = 0x63;
+        squeezeLabel[1] = 0x31;
+
+        ScalarFromUniformLib.Curve curve = ScalarFromUniformLib.curveGrumpkin();
+        uint256 output;
+        (transcript, output) = KeccakTranscriptLib.squeeze(transcript, curve, squeezeLabel);
+
+        uint256 expected = Field.reverse256(0xd12b7cd39aa2fc3af9bfd4f1dfd8ffa6498f57e35021675f4227d448b5540922);
+        assertEq(output, expected);
+
+        uint256 s3 = 128;
+        uint8[] memory label3 = new uint8[](2); // b"s3" in Rust
+        label3[0] = 0x73;
+        label3[1] = 0x33;
+
+        transcript = KeccakTranscriptLib.absorb(transcript, label3, s3);
+
+        // b"c2" in Rust
+        squeezeLabel[0] = 0x63;
+        squeezeLabel[1] = 0x32;
+        (, output) = KeccakTranscriptLib.squeeze(transcript, curve, squeezeLabel);
+
+        expected = Field.reverse256(0xfb894998c48dd652b32a109d3d2e579a0878f7f5cacfb572dc21666b9cfe221a);
+        assertEq(output, expected);
+    }
+
+    function testKeccakTranscriptIPAGrumpkinComputations() public {
+        // b"TestEval" in Rust
+        uint8[] memory label = new uint8[](8);
+        label[0] = 0x54;
+        label[1] = 0x65;
+        label[2] = 0x73;
+        label[3] = 0x74;
+        label[4] = 0x45;
+        label[5] = 0x76;
+        label[6] = 0x61;
+        label[7] = 0x6c;
+        KeccakTranscriptLib.KeccakTranscript memory transcript = KeccakTranscriptLib.instantiate(label);
+
+        // b"IPA" in Rust
+        label = new uint8[](3);
+        label[0] = 0x49;
+        label[1] = 0x50;
+        label[2] = 0x41;
+
+        transcript = KeccakTranscriptLib.dom_sep(transcript, label);
+
+        // b"U" in Rust
+        label = new uint8[](1);
+        label[0] = 0x55;
+
+        uint8[] memory U_bytes = new uint8[](97);
+        U_bytes[0] = 0x05;
+        U_bytes[1] = 0xdf;
+        U_bytes[2] = 0xbd;
+        U_bytes[3] = 0xc0;
+        U_bytes[4] = 0x8a;
+        U_bytes[5] = 0x45;
+        U_bytes[6] = 0x2d;
+        U_bytes[7] = 0x84;
+        U_bytes[8] = 0xe9;
+        U_bytes[9] = 0x8e;
+        U_bytes[10] = 0xa0;
+        U_bytes[11] = 0x63;
+        U_bytes[12] = 0x21;
+        U_bytes[13] = 0x1a;
+        U_bytes[14] = 0x16;
+        U_bytes[15] = 0xf9;
+        U_bytes[16] = 0x00;
+        U_bytes[17] = 0x16;
+        U_bytes[18] = 0xb3;
+        U_bytes[19] = 0x1e;
+        U_bytes[20] = 0xfe;
+        U_bytes[21] = 0x89;
+        U_bytes[22] = 0xf6;
+        U_bytes[23] = 0x3f;
+        U_bytes[24] = 0xbe;
+        U_bytes[25] = 0x38;
+        U_bytes[26] = 0x2b;
+        U_bytes[27] = 0x1a;
+        U_bytes[28] = 0x59;
+        U_bytes[29] = 0x68;
+        U_bytes[30] = 0x72;
+        U_bytes[31] = 0x1e;
+        U_bytes[32] = 0x81;
+        U_bytes[33] = 0x52;
+        U_bytes[34] = 0x6d;
+        U_bytes[35] = 0xf9;
+        U_bytes[36] = 0xc0;
+        U_bytes[37] = 0xf9;
+        U_bytes[38] = 0x9b;
+        U_bytes[39] = 0x11;
+        U_bytes[40] = 0x5b;
+        U_bytes[41] = 0x02;
+        U_bytes[42] = 0x36;
+        U_bytes[43] = 0x95;
+        U_bytes[44] = 0x3f;
+        U_bytes[45] = 0x02;
+        U_bytes[46] = 0x90;
+        U_bytes[47] = 0x54;
+        U_bytes[48] = 0x78;
+        U_bytes[49] = 0x43;
+        U_bytes[50] = 0x0d;
+        U_bytes[51] = 0x10;
+        U_bytes[52] = 0xa5;
+        U_bytes[53] = 0xab;
+        U_bytes[54] = 0xe1;
+        U_bytes[55] = 0x35;
+        U_bytes[56] = 0x01;
+        U_bytes[57] = 0x3f;
+        U_bytes[58] = 0x2c;
+        U_bytes[59] = 0x59;
+        U_bytes[60] = 0xc0;
+        U_bytes[61] = 0x70;
+        U_bytes[62] = 0x30;
+        U_bytes[63] = 0x1f;
+        U_bytes[64] = 0x01;
+        U_bytes[65] = 0xbf;
+        U_bytes[66] = 0x33;
+        U_bytes[67] = 0x6d;
+        U_bytes[68] = 0x8e;
+        U_bytes[69] = 0xd8;
+        U_bytes[70] = 0x20;
+        U_bytes[71] = 0x4d;
+        U_bytes[72] = 0x26;
+        U_bytes[73] = 0xf3;
+        U_bytes[74] = 0xe6;
+        U_bytes[75] = 0x05;
+        U_bytes[76] = 0x51;
+        U_bytes[77] = 0xca;
+        U_bytes[78] = 0x7f;
+        U_bytes[79] = 0xa4;
+        U_bytes[80] = 0xe7;
+        U_bytes[81] = 0x23;
+        U_bytes[82] = 0x34;
+        U_bytes[83] = 0x6a;
+        U_bytes[84] = 0x8e;
+        U_bytes[85] = 0x7c;
+        U_bytes[86] = 0xea;
+        U_bytes[87] = 0xb7;
+        U_bytes[88] = 0x4a;
+        U_bytes[89] = 0x7a;
+        U_bytes[90] = 0x9a;
+        U_bytes[91] = 0x8e;
+        U_bytes[92] = 0x7e;
+        U_bytes[93] = 0x2a;
+        U_bytes[94] = 0x66;
+        U_bytes[95] = 0x14;
+        U_bytes[96] = 0x25;
+
+        transcript = KeccakTranscriptLib.absorb(transcript, label, U_bytes);
+
+        // b"r" in Rust
+        label = new uint8[](1);
+        label[0] = 0x72;
+        uint256 r;
+        (transcript, r) = KeccakTranscriptLib.squeeze(transcript, ScalarFromUniformLib.curveGrumpkin(), label);
+
+        uint256 r_expected = 0x26dbed0c0b99929de85b3c8d4c501f5216b0c3fa951b92afb75b6044c7b6885d;
+        assertEq(r_expected, r);
     }
 }
